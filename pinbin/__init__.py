8d086d745ee08adf1b9825792c501c26b37974bf
Hung Truong <hung@hung-truong.com>
2014-04-19 18:40:09 -0400

    heroku mongodb stuff

--- pinbin/__init__.py
+++ pinbin/__init__.py
@@ -1,9 +1,26 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+import os
+
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {'DB': "pinbin"}
 app.config["SECRET_KEY"] = "HIMOM"
+
+MONGO_URL = os.environ.get("MONGOHQ_URL")
+
+if MONGO_URL:
+    credentials = re.sub(r"(.*?)//(.*?)(@hatch)", r"\2",MONGO_URL)
+    username = credentials.split(":")[0]
+    password = credentials.split(":")[1]
+    app.config["MONGODB_DB"] = MONGO_URL.split("/")[-1]
+    connect(
+        MONGO_URL.split("/")[-1],
+        host=MONGO_URL,
+        port=1043,
+        username=username,
+        password=password
+    )
 
 db = MongoEngine(app)
 
